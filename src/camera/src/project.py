5b04672619f74ed2ca3d86a2995f623ab95f5a75
Theo <theo.guegan@etu.utc.fr>
2024-06-20 08:19:38 +0200

    re-indent code

--- src/camera/src/project.py
+++ src/camera/src/project.py
@@ -6,10 +6,11 @@ import cv2
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image, CameraInfo
 
+
 class CameraNode:
     def __init__(self):
         # Creates a node called and registers it to the ROS master
-        rospy.init_node('project')
+        rospy.init_node("project")
 
         # CvBridge is used to convert ROS messages to matrices manipulable by OpenCV
         self.bridge = CvBridge()
@@ -20,49 +21,55 @@ class CameraNode:
         self.P = self.K = self.D = None
 
         # Constants for the checkerboard detection and projection
-        self.board_size = (8,6)
+        self.board_size = (8, 6)
         self.objp = np.zeros((np.prod(self.board_size), 3), np.float32)
-        self.objp[:,:2] = np.mgrid[:self.board_size[0],:self.board_size[1]].T.reshape(-1,2)
+        self.objp[:, :2] = np.mgrid[
+            : self.board_size[0], : self.board_size[1]
+        ].T.reshape(-1, 2)
 
         # Publisher to the output topics.
-        self.pub_img = rospy.Publisher('~output', Image, queue_size=1)
+        self.pub_img = rospy.Publisher("~output", Image, queue_size=1)
 
         # Subscriber to the input topic. self.callback is called when a message is received
-        self.subscriber_info = rospy.Subscriber('/turtlebotcam/camera_info', CameraInfo, self.callback_cam_info)
-        self.subscriber_imag = rospy.Subscriber('/camera/image_rect_color', Image, self.callback, queue_size=1)
+        self.subscriber_info = rospy.Subscriber(
+            "/turtlebotcam/camera_info", CameraInfo, self.callback_cam_info
+        )
+        self.subscriber_imag = rospy.Subscriber(
+            "/camera/image_rect_color", Image, self.callback, queue_size=1
+        )
 
     def callback_cam_info(self, msg):
-        '''
+        """
         Function called when a CameraInfo message is received.
         Retrieves the camera intrinsic parameters from the ROS calibration.
         Once the parameters are received, the topic is not listened to anymore
         msg: CameraInfo message received
-        '''
-        self.P = np.array([msg.P[0:4],msg.P[4:8],msg.P[8:12]])
-        self.K = np.resize(3,3)
-        self.D = np.resize(5,1)
+        """
+        self.P = np.array([msg.P[0:4], msg.P[4:8], msg.P[8:12]])
+        self.K = np.resize(3, 3)
+        self.D = np.resize(5, 1)
 
     def drawAxes(self, img, axis):
-        '''
+        """
         Function called when an image message is received.
         Retrieves the camera intrinsic parameters from the ROS calibration.
         Once the parameters are received, the topic is not listened to anymore
         img: OpenCV/numpy matrix
         axis: 2x4 matrix describing the coordinates of the origin and 3 axes in image space
         return: OpenCV/numpy matrix with lines drawn on it
-        '''
+        """
         axis = axis.astype(int)
-        img = cv2.line(img, tuple(axis[:,0]), tuple(axis[:,1]), (255,0,0), 5)
-        img = cv2.line(img, tuple(axis[:,0]), tuple(axis[:,2]), (0,255,0), 5)
-        img = cv2.line(img, tuple(axis[:,0]), tuple(axis[:,3]), (0,0,255), 5)
+        img = cv2.line(img, tuple(axis[:, 0]), tuple(axis[:, 1]), (255, 0, 0), 5)
+        img = cv2.line(img, tuple(axis[:, 0]), tuple(axis[:, 2]), (0, 255, 0), 5)
+        img = cv2.line(img, tuple(axis[:, 0]), tuple(axis[:, 3]), (0, 0, 255), 5)
         return img
 
     def callback(self, msg):
-        '''
+        """
         Function called when an image is received.
         msg: Image message received
         img: Width*Height*3 Numpy matrix storing the image
-        '''
+        """
         # Convert ROS Image -> OpenCV
         try:
             img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
@@ -71,23 +78,27 @@ class CameraNode:
             return
 
         # As we use the calibration information from ROS, die if it does not exist
-        if self.P is None or self.P[0,0]==0:
-            rospy.logwarn('Camera is not calibrated')
+        if self.P is None or self.P[0, 0] == 0:
+            rospy.logwarn("Camera is not calibrated")
             return
 
-        #u,v,w = np.matmul(self.P,np.array([[0.005],[0],[0.01],[1]]))
-        #cv2.circle(img, (int(u/w),int(v/w)), 5, (0,0,255), -1)
-        
+        # u,v,w = np.matmul(self.P,np.array([[0.005],[0],[0.01],[1]]))
+        # cv2.circle(img, (int(u/w),int(v/w)), 5, (0,0,255), -1)
+
         cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        retval , corners = cv2.findChessboardCorners(img, self.board_size)
-        
-        cv2.cornerSubPix(img, corners, self.board_size, (-1,-1), (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001))
+        retval, corners = cv2.findChessboardCorners(img, self.board_size)
+
+        cv2.cornerSubPix(
+            img,
+            corners,
+            self.board_size,
+            (-1, -1),
+            (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001),
+        )
         retval, rvec, tvec = cv2.solvePnP(self.objp, corners, self.K, self.D)
         print(rvec)
         print(tvec)
         print("------")
-        
-        
 
         # Convert OpenCV -> ROS Image and publish
         try:
@@ -95,7 +106,8 @@ class CameraNode:
         except CvBridgeError as e:
             rospy.logwarn(e)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # Start the node and wait until it receives a message or stopped by Ctrl+C
     node = CameraNode()
     rospy.spin()
--- src/mapping/src/mapping.py
+++ src/mapping/src/mapping.py
@@ -9,65 +9,87 @@ from std_msgs.msg import Float32
 from sensor_msgs.point_cloud2 import create_cloud, read_points
 from tf.transformations import euler_from_quaternion
 
-PC2FIELDS = [PointField('x', 0, PointField.FLOAT32, 1),
-             PointField('y', 4, PointField.FLOAT32, 1),
-             PointField('z', 8, PointField.FLOAT32, 1),
-             PointField('c', 12, PointField.INT16, 1)
+PC2FIELDS = [
+    PointField("x", 0, PointField.FLOAT32, 1),
+    PointField("y", 4, PointField.FLOAT32, 1),
+    PointField("z", 8, PointField.FLOAT32, 1),
+    PointField("c", 12, PointField.INT16, 1),
 ]
 
+
 def norm(p1, p2):
-    return ((p1[0]-p2[0])**2+(p1[1]-p2[1])**2)**(1/2)
+    return ((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2) ** (1 / 2)
 
 
 class MappingNode:
     def __init__(self):
-        rospy.init_node('mapping')
+        rospy.init_node("mapping")
         # Map
         self.map = np.array([])
-        
-        
 
         # Publishers
-        self.mapper = rospy.Publisher('/map', PointCloud2, queue_size=10)
-        
+        self.mapper = rospy.Publisher("/map", PointCloud2, queue_size=10)
+
         # Subscribers
-        self.odom = rospy.Subscriber('/pose_final', PoseStamped, self.callback_odom)
-        self.lidar = rospy.Subscriber('/lidar/clusters', PointCloud2, self.callback_final)
+        self.odom = rospy.Subscriber("/pose_final", PoseStamped, self.callback_odom)
+        self.lidar = rospy.Subscriber(
+            "/lidar/clusters", PointCloud2, self.callback_final
+        )
 
     def callback_odom(self, odom):
         self.x_robot = odom.pose.position.x
         self.y_robot = odom.pose.position.y
-        self.theta_robot = euler_from_quaternion([odom.pose.orientation.x, odom.pose.orientation.y, odom.pose.orientation.z, odom.pose.orientation.w])[2]
+        self.theta_robot = euler_from_quaternion(
+            [
+                odom.pose.orientation.x,
+                odom.pose.orientation.y,
+                odom.pose.orientation.z,
+                odom.pose.orientation.w,
+            ]
+        )[2]
         return
-    
+
     def callback_vitesse(self, msg):
         self.angle = msg.data
         return
-    
-    def callback_final(self, msg):    
+
+    def callback_final(self, msg):
         # Transformation matrix
-        T = np.array([[np.cos(self.theta_robot), -np.sin(self.theta_robot), self.x_robot],
-                      [np.sin(self.theta_robot), np.cos(self.theta_robot), self.y_robot],
-                      [0, 0, 1]])
-        
+        T = np.array(
+            [
+                [np.cos(self.theta_robot), -np.sin(self.theta_robot), self.x_robot],
+                [np.sin(self.theta_robot), np.cos(self.theta_robot), self.y_robot],
+                [0, 0, 1],
+            ]
+        )
+
         # Transform the points from the clusters
-        points = np.array(list(read_points(msg)))[:,:2]
-        points = np.hstack((points, np.ones((points.shape[0],1)))).transpose()
+        points = np.array(list(read_points(msg)))[:, :2]
+        points = np.hstack((points, np.ones((points.shape[0], 1)))).transpose()
         points = np.dot(T, points)[:2].transpose()
 
         # For debugging purposes
-        points_msg = create_cloud(msg.header, PC2FIELDS, [[points[i,0],points[i,1],0,0] for i in range(points.shape[0])])
+        points_msg = create_cloud(
+            msg.header,
+            PC2FIELDS,
+            [[points[i, 0], points[i, 1], 0, 0] for i in range(points.shape[0])],
+        )
 
         # Add points to the map
         self.map = np.vstack((self.map, points)) if self.map.size else points
-        
-        map_msg = create_cloud(msg.header, PC2FIELDS, [[self.map[i,0],self.map[i,1],0,0] for i in range(self.map.shape[0])])
+
+        map_msg = create_cloud(
+            msg.header,
+            PC2FIELDS,
+            [[self.map[i, 0], self.map[i, 1], 0, 0] for i in range(self.map.shape[0])],
+        )
         map_msg.header.frame_id = "base_scan"
 
         self.mapper.publish(map_msg)
         return
-        
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     node = MappingNode()
     try:
         rospy.spin()
